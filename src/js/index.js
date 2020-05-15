57d0bb1871ff20895cd8aca5c33b233a37828386
Aleksandra Jurek <aleksandra.jurek@closbrothers.pl>
2020-05-15 15:53:36 +0200

    test patch mode and diff

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 const roman = 'roman';
-console.log(roman);
+// roman = 'roman2';
+const friends = ['bibi', 'lala'];
+
+friends[0] = 'hyra';
+// friends = [];
+// friends[0] = null;
+friends.pop();
+
+const say = {
+  yes() {
+    console.log('yes');
+  },
+  no() {
+    console.log('no');
+  },
+  dontKnow() {
+    console.log('dont know');
+  },
+};
+console.log(friends, roman);
+console.log(say.no());
